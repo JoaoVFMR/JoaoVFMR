19a8c03bcb78fb6b6c09a4d5bb75638d4f1f66b5
JoaoVMFR <joao.vitor.rocha.2508@gmail.com>
2024-08-16 19:45:54 -0300

    Excluindo as cópias fora da pasta

--- atividade2.js
+++
@@ -1,7 +0,0 @@
-import PromptSync from "prompt-sync"
-const prompt = PromptSync()
-
-let numero = Number(prompt("Digite o valor da tabuada: "))
-for (let i = 1; i < 11; i++){
-    console.log(numero, "*", i, "=", numero * i)
-}
