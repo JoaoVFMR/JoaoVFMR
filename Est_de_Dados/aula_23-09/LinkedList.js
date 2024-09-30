3f7472c76e26f0583917ee342d317b7fd339ad9b
JoaoVFMR <joao.vitor.rocha.2508@gmail.com>
2024-09-30 09:01:45 -0300

    Aula do dia 30/09

--- Est_de_Dados/aula_23-09/LinkedList.js
+++ Est_de_Dados/aula_23-09/LinkedList.js
@@ -24,16 +24,33 @@ class LinkedList {
         this.count++
     }
 
-    insert(element, position) {
+    insert(element, index) {
         // Insere um elemento na posição indicada
+        if (index >= 0 && index <= this.count) {
+            const node = new Node(element)
+            if (index == 0) {
+                const current = this.head
+                node.next = current
+                this.head = node
+            } else {
+                const previous = this.getElementAt(index - 1)
+                const current = previous.next
+                node.next = current
+                previous.next = node
+            }
+            this.count++
+            return true
+        }
+        return false
     }
+
 
     getElementAt(index) {
         // Devolve o elemento presente na posição indicada. Caso seja vazio, retorna "undefined"
         if (index >= 0 && index <= this.count) {
             let node = this.head
             for (let i = 0; i < index && node != null; i++) {
-            node = node.next
+                node = node.next
             }
             return node
         }
@@ -42,10 +59,20 @@ class LinkedList {
 
     remove(element) {
         // Remove o elemento indicado
+        const index = this.index0f(element)
+        return this.removeAt(index)
     }
 
     index0f(element) {
         // Devolve o índice de determinado elemento, se o elemento não estiver na lista, retorna "-1"
+        let current = this.head
+        for (let i = 0; i < this.count && current != null; i++) {
+            if (this.equalsFn(element, current.element)) {
+                return i
+            }
+            current = current.next
+        }
+        return -1
     }
 
     removeAt(index) {
@@ -70,15 +97,30 @@ class LinkedList {
 
     isEmpty() {
         // Verifica se a lista está vazia
+        return this.size() === 0
     }
 
     size() {
         // Retorna a quantidade de elementos presentes na lista (semelhante ao length)
+        return this.count
+    }
+
+    getHead() {
+        return this.head
     }
 
     toString() {
         // Devolve uma representação em string da lista ligada
-
+        if (this.head == null) {
+            return ''
+        }
+        let objString = `${this.head.element}`
+        let current = this.head.next
+        for (let i = 1; i < this.size() && current != null; i++) {
+            objString = `${objString},${current.element}`
+            current = current.next
+        }
+        return objString          
     }
 }
 
